83c3fb91ee19ffe46ea20dfe6011ab6871f1e6ee
Kerry Gilbert <kerry@punkave.com>
2013-09-23 16:33:41 -0400

    basic support for text fields, selects, checkboxes and radio buttons. additions to readme

--- README.md
+++ README.md
@@ -1,4 +1,61 @@
 crudbud
 =======
 
-A no-hassle, easy-to-install CRUD interface creator for express
+A no-hassle, easy-to-install CRUD interface creator for express.
+
+
+#### Are you:
+New to express? A frontend developer looking to build out a backend interface? A backend developer looking to save some time?
+
+Crudbud sets up your rest routes
+
+
+## Usage
+### In app.js
+```javascript
+var express = require('express')
+  , crudbud = require('crudbud');
+  , app = express();
+
+app.condfigure(function() {
+  // blah blah blah
+})
+
+crudbud(app, {
+  models: {
+    blogPost: {
+      fields: {
+        title: { type: String }
+        body: { type: String, widget: 'textarea' },
+      }
+    }
+  }
+});
+
+```
+
+## Form Field Widget Types
+
+* Text
+* Checkbox
+* Select
+* Radio Group
+* Date
+
+
+```javascript
+crudbud(app, {
+  dbName: 'myApp',
+  models: {
+    person: {
+      fields: {
+        name: { type: String },
+        age: { type: Number },
+        sex: { type: String, choices: ['male','female'], widget: 'multipleRadio' },
+        eyeColor: { type: String, choices: ['blue','brown','green','hazel'] },
+        isMarried: { type: Boolean }
+      }
+    }
+  }
+});
+```
---
+++ index.js
@@ -0,0 +1,94 @@
+var async = require('async')
+  , $ = require('jquery')
+  , _ = require('underscore')
+  , mongoose = require('mongoose')
+  , forms = require('forms')
+  , fields = forms.fields
+  , widgets = forms.widgets
+  , mongo = require('./db/mongo-store');
+
+var schemas = {};
+var schemaNames = [];
+var classes = {};
+
+module.exports = function(app, options) {
+  mongo({ dbName: options.dbName || null });
+
+  var models = options.models;
+
+
+  // create a mongoose schema for each of our user defined models\
+  _.each(options.models, function(model, modelName){
+    // I will eventually want to add some keys to model.fields.
+    // Probably will be auto-generating slug and createdAt
+    model.formFields = {};
+
+    _.each(model.fields, function(field, f){
+      var field = model.fields[f];
+      var choices;
+      var widget;
+
+      if(field.choices) {
+        choices = {};
+        _.each(field.choices, function(choice, c){
+          choices[choice] = choice;
+        });
+
+        widget = widgets.select();
+      }
+
+      if(field.widget) {
+        widget = widgets[field.widget]();
+      }
+
+      model.formFields[f] = fields[field.type.name.toLowerCase()]({
+        required: field.required || false,
+        choices: choices || null,
+        widget: widget || null
+      });
+
+      delete model.fields[f].widget;
+      delete model.fields[f].choices;
+    })
+
+    schemas[modelName] = mongoose.Schema(model.fields);
+    schemaNames.push(modelName);
+    module.exports[modelName] = classes[modelName] = mongoose.model(modelName, schemas[modelName]);
+  });
+
+  
+  // loop through each of our models and create routes for them
+  async.eachSeries(schemaNames, function(modelName, callback) {
+    // we use Caolan's forms module to generate markup for our
+    var formFields = forms.create(models[modelName].formFields);
+
+    var $form = $('<form action="/new/'+modelName+'" method="POST"></form>');
+    $form.append(formFields.toHTML());
+    $form.append('<input type="submit">');
+
+    var markup = $('<div>').append($form).clone().html(); 
+
+    app.get('/new/'+modelName, function(req, res) {
+      res.send(markup);
+    });
+
+    app.post('/new/'+modelName, function(req, res) {
+      var instance = new classes[modelName](req.body);
+
+      instance.save(function(err){
+        console.log(err)
+        res.send(req.body)
+      });
+    });
+
+    app.get('/'+modelName+'/json', function(req, res){
+      classes[modelName].find({}, function(err, items){
+        res.send(items);
+      });
+    });
+
+    callback();
+  }, function(err) {
+    console.log('Crudbud initialized');
+  });
+}
